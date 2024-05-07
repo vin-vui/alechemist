5b6ea3fcadd8de278a68deb0562a3b2de2fd4d7d
vinvui <admin@vinvui.com>
2024-05-07 17:45:46 +0200

    Hotfix #6

--- database/migrations/2023_09_28_082947_create_brewing_steps_table.php
+++ database/migrations/2023_09_28_082947_create_brewing_steps_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Brewing_Steps', function (Blueprint $table) {
+        Schema::create('brewing_steps', function (Blueprint $table) {
             $table->id();
             $table->float('quantity');
             $table->enum('unit', ['g', 'l', '°C']);
