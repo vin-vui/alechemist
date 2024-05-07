05ded439fa7023eaa45792843c545f1b00a40ffd
vinvui <admin@vinvui.com>
2024-05-07 17:16:18 +0200

    Hotfix #2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#000000">
+    <meta name="author" content="vinvui">
+    <link rel="canonical" href="{{ url('/') }}">
+    <link rel="shortcut icon" href="{{ url('favicon.ico') }}" type="image/x-icon">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
@@ -10,6 +16,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel='manifest' href='{{ asset('manifest.json') }}'>
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
